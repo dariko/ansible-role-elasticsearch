25ff2d5bf04191a062256d9f2743926ba65427c6
dariko <git@dariozanzico.com>
2017-05-10 10:29:02 +0200

    disable/enable allocation on restart

--- files/restart_and_wait_for_cluster_green.sh
+++ files/restart_and_wait_for_cluster_green.sh
@@ -5,8 +5,36 @@ unset https_proxy
 unset HTTP_PROXY
 unset HTTPS_PROXY
 
+# disable shard allocation
+curl -XPUT localhost:9200/_cluster/settings -d '
+  {
+    "transient": {
+      "cluster.routing.allocation.enable": "none"
+    }
+  }
+'
+# flush
+curl -XPOST localhost:9200/_flush/synced
+sleep 2
+
 systemctl restart elasticsearch
-sleep 1
+sleep 2
+
+# wait for tcp9200
+while ! curl localhost:9200
+do
+    sleep 1
+done
+sleep 5
+
+# enable shard allocation
+curl -XPUT localhost:9200/_cluster/settings -d '
+  {
+    "transient": {
+      "cluster.routing.allocation.enable": "all"
+    }
+  }
+'
 
 while ! curl -s localhost:9200/_cluster/health|jq -r '.status' |grep -q green
 do
